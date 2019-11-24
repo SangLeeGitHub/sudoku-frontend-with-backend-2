dcf5c8179cab1a1458e5ca4f59453d62203ad9d6
Slime@MBP11,2 <hotdeveloper@outlook.com>
2019-11-24 11:35:40 -0500

    Revise backend.spec.js

--- spec/backend.spec.js
+++ spec/backend.spec.js
@@ -45,6 +45,30 @@ describe("Sudoku API Exists", function() {
                     expect(line.sort().join() == testVal).toBeTruthy();
                     line = [];
                 }
+
+                done();
+            });
+        });
+
+        it("Test 3x3 segment rule", function(done) {
+            request.get(base_url, function(error, response, body) {
+
+                var t = JSON.parse(body);
+
+                var segment = [];
+
+                for (var y = 0; y < 3; y++) {
+                    for (var x = 0; x < 3; x++) {
+                        for (var i = 0; i < 3; i++) {
+                            for (var j = 0; j < 3; j++) {
+                                segment.push(t[j + (i * 9) + (x * 3) + (y * 27)]);
+                            }
+                        }
+                        expect(segment.sort().join() == testVal).toBeTruthy();
+                        segment = [];
+                    }
+                }
+
                 backend.closeServer();
                 done();
             });
